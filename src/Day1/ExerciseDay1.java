bef98de9de20240972d27e67a8637a064bca8f8e
raisa <raisa.yasmina29@gmail.com>
2024-09-03 20:53:48 +0700

    update

--- src/Day1/ExerciseDay1.java
+++ src/Day1/ExerciseDay1.java
@@ -1,4 +1,58 @@
 package Day1;
 
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
 public class ExerciseDay1 {
+    public static void runExerciseD1() {
+        //Exercise 1
+        System.out.println("Exercise 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Length: ");
+        double length = sc.nextDouble();
+        System.out.println("Width: ");
+        double width = sc.nextDouble();
+        System.out.println("The length of the rectangle is: "+ (length * width));
+        System.out.println();
+
+        //Exercise 2
+        System.out.println("Exercise 2");
+        System.out.println("Radius of circle: ");
+        double radius = sc.nextDouble();
+        System.out.println("Diameter: " + radius * 2);
+        System.out.println("Circumference: " + radius * 2 * Math.PI);
+        System.out.println("Area: " + Math.PI * radius * radius);
+        System.out.println();
+
+        //Exercise 3
+        System.out.println("Exercise 3");
+        System.out.println("Angle a corner: ");
+        double a = sc.nextDouble();
+        System.out.println("Angle b corner: ");
+        double b = sc.nextDouble();
+        double c = 180 - (a + b);
+        System.out.println("Angle of the triangle is: " + c);
+        System.out.println();
+
+        //Exercise 4
+        System.out.println("Exercise 4");
+        LocalDate date1 = LocalDate.of(2024, 3, 19);
+        LocalDate date2 = LocalDate.of(2024, 3, 21);
+        long diff = ChronoUnit.DAYS.between(date1, date2);
+        System.out.println(diff);
+        System.out.println();
+
+        //Exercise 5
+        System.out.println("Exercise 5");
+        String name = "John Doe";
+        System.out.println("Initials: " + getInitials(name));
+    }
+
+    public static String getInitials(String name) {
+        int lastname = name.lastIndexOf(' ');
+        return name.substring(0,1) + name.substring(lastname + 1, lastname + 2);
+    }
 }
+
--- src/Day2/ExerciseDay2.java
+++ src/Day2/ExerciseDay2.java
@@ -1,4 +1,54 @@
 package Day2;
 
+import java.util.Scanner;
+
 public class ExerciseDay2 {
+    public static void runExerciseD2() {
+        //Exercise 1
+        System.out.println("Exercise 1");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Weather Degree: ");
+        double weatherinF = sc.nextDouble();
+        System.out.println("The weather in Celcius is: " + ((weatherinF - 32) * (5.0 / 9.0)));
+        System.out.println();
+
+        //Exercise 2
+        System.out.println("Exercise 2");
+        System.out.println("Distance in centimeter: ");
+        double numCM = sc.nextDouble();
+        System.out.println("The distance in Kilometer is: " + (numCM / 100000.0));
+        System.out.println();
+
+        //Exercise 3
+        System.out.println("Exercise 3");
+        System.out.println("Input amount: ");
+        double n = sc.nextDouble();
+        boolean isOdd = false;
+        if (n % 2 == 0){
+            System.out.printf("Rp%,.2f", n);
+        }
+        else {
+            System.out.println(isOdd);
+        }
+        System.out.println();
+
+        //Exercise 4
+        System.out.println("Exercise 4");
+        String word = "Hello world";
+        String newword = word.replace(word.substring(1,4), "");
+        System.out.println(newword);
+        System.out.println();
+
+        //Exercise 5
+        System.out.println("Exercise 5");
+        System.out.println("Is it Palindrome? ");
+        String wordPal = sc.next();
+        System.out.println(isPalindrome(wordPal));
+
+    }
+    public static boolean isPalindrome(String x) {
+        StringBuilder sb = new StringBuilder(x);
+        sb.reverse();
+        return x.equals(sb.toString());
+    }
 }
