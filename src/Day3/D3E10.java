87e5bb136882cd1f87b985f5a4e89e10267e3994
ryasmina29 <raisa.yasmina29@gmail.com>
2024-09-04 21:52:38 +0700

    Update D3E10.java

--- src/Day3/D3E10.java
+++ src/Day3/D3E10.java
@@ -8,7 +8,6 @@ public class D3E10 {
     public static void RunD3E10() {
         int[] num = {5, 3, 8, 1, 2, 9};
         Arrays.sort(num);
-        int x = Array.getLength(num);
 
         System.out.println(num[1]);
     }
