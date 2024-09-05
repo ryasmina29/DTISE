bb4764ea0ab6a812ead71300e348b75f1158da66
ryasmina29 <raisa.yasmina29@gmail.com>
2024-09-06 00:00:33 +0700

    upd

---
+++ src/Day3/D3E10.java
@@ -0,0 +1,22 @@
+package Day3;
+
+//Second smallest
+public class D3E10 {
+    public static void RunD3E10() {
+        int[] num = {5, 3, 8, 1, 2, 9};
+
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num.length; j++) {
+                int x = 0;
+                if (num[i] < num[j]) { //Change here for descend/ascend
+                    x = num[i];
+                    num[i] = num[j];
+                    num[j] = x;
+                }
+            }
+        }
+
+        System.out.println(num[1]);
+    }
+}
